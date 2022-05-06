67396d0b015c518436be56eb6b2ed0f20badd82a
Tina <tina.zhou.hui@gmail.com>
2022-05-06 12:28:10 +0100

    dev: testing linting

--- local.ts
+++ local.ts
@@ -3,5 +3,6 @@ import app from './src/app';
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {  // initialize server
+  const helo='hey';
   console.log(`I am up and running at http://localhost:${PORT}`);
 });
