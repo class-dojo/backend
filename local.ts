96eacf5104b45d2f93cffd09a9450ee7f22a59b6
Tina <tina.zhou.hui@gmail.com>
2022-05-06 12:22:38 +0100

    dev: testing husky

--- local.ts
+++ local.ts
@@ -3,6 +3,6 @@ import app from './src/app';
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {  // initialize server
-    const hello = 'asdfsadf';
+  const hello = 'asdfsdf';
   console.log(`I am up and running at http://localhost:${PORT}`);
 });
