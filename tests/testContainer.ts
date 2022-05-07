540752f56948890bc9c3305a7fa8f8618cb790bd
Tina <tina.zhou.hui@gmail.com>
2022-05-07 17:56:30 +0100

    dev: dummy controller and tests

---
+++ tests/testContainer.ts
@@ -0,0 +1,14 @@
+import {ContainerBuilder, YamlFileLoader} from 'node-dependency-injection';
+import {join} from 'path';
+
+export const containerBuilder = (): ContainerBuilder => {
+  const srcDir = join(__dirname, '../src');
+  const container = new ContainerBuilder(true, srcDir);
+
+  const loader = new YamlFileLoader(container);
+  loader.load(__dirname + '/../src/config/services.yml');
+
+  container.compile();
+
+  return container;
+};
