ad3469ce959a3f8d0b8fdd86c8650f351fb11ca0
Tina <tina.zhou.hui@gmail.com>
2022-05-09 19:07:59 +0100

    refactor: cleanup

--- local.ts
+++ local.ts
@@ -1,11 +1,6 @@
 import app from './src/app';
-import {generateS3PutLink} from './src/models/s3Links';
 
 const PORT = process.env.PORT || 5000;
-
-const link = generateS3PutLink();
-
-console.log(link);
 
 app.listen(PORT, () => {  // initialize server
   console.log(`I am up and running at http://localhost:${PORT}`);
