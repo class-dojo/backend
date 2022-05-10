a2196c3e73d996d0ec4051ef090b31ca3349d058
Fav8 <leonardo.damico7597@gmail.com>
2022-05-10 13:54:22 +0200

    feat dataManipulation

--- src/models/SentimentModel.ts
+++ src/models/SentimentModel.ts
@@ -1,7 +1,26 @@
 import BaseModel from './BaseModel';
 import RekognitionModel from './RekognitionModel';
+import {AmountOfPeople, calculateAttentionScore, calculateMoodScore, removeUselessProps} from '../models/helpers';
+import { IFaceDetails } from './interface';
 
 export default class SentimentModel extends BaseModel {
   constructor (private readonly rekognitionModel: RekognitionModel) {
     super();
-  }}
+  }
+  manipulateData (facesArray : IFaceDetails[]) {
+    let moodScore = 0;
+    let attentionScore = 0;
+    for (let face of facesArray) {
+      face = removeUselessProps(face);
+      moodScore += calculateMoodScore(face);
+      attentionScore = calculateAttentionScore(face);
+    }
+    const amountOfPeople = AmountOfPeople(facesArray);
+    moodScore = parseFloat((moodScore / amountOfPeople).toFixed(2));
+    attentionScore = parseFloat((attentionScore / amountOfPeople).toFixed(2));
+    const result = {
+      attentionScore,
+      moodScore,
+      amountOfPeople
+    }; return result;}
+}
---
+++ src/models/interface.ts
@@ -0,0 +1,12 @@
+export interface IRecognitionResponse {
+  FaceDetails: IFaceDetails[];
+ }
+
+export interface IFaceDetails {
+   Emotions: IEmotion[],
+ }
+
+export interface IEmotion {
+     Type: string,
+     Confidence: number
+ }
