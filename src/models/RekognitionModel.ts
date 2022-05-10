bda3eda6d7251f7b3cc42d6e64824eb771ffefc5
Fav8 <leonardo.damico7597@gmail.com>
2022-05-10 14:09:26 +0200

    test- mock faceAnalys return

--- src/models/RekognitionModel.ts
+++ src/models/RekognitionModel.ts
@@ -2,6 +2,8 @@ import BaseModel from './BaseModel';
 import RekognitionConnection from '../components/RekognitionConnection';
 import {Rekognition} from 'aws-sdk';
 import BaseRekognitionConnection from '../components/BaseRekognitionConnection';
+import * as mockResponse from '../../tests/mockResponses/mockResponse1.json';
+
 
 export default class RekognitionModel extends BaseModel {
   private readonly rekognition: Rekognition;
@@ -12,7 +14,7 @@ export default class RekognitionModel extends BaseModel {
   }
 
   detectFaces (imageIds: string[]) {
-    return;
+    return mockResponse;
     //this.rekognition.detectFaces();
   }
 
--- src/models/SentimentModel.ts
+++ src/models/SentimentModel.ts
@@ -4,4 +4,8 @@ import RekognitionModel from './RekognitionModel';
 export default class SentimentModel extends BaseModel {
   constructor (private readonly rekognitionModel: RekognitionModel) {
     super();
-  }}
+  }
+  analyzeImages (images : string[]) {
+    return this.rekognitionModel.detectFaces(images);
+  }
+}
