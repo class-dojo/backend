5c35ae61801ab9d8d3fb2e440aa3d1b0e333d842
Tina <tina.zhou.hui@gmail.com>
2022-05-10 17:29:11 +0100

    feat: swagger ui

--- local.ts
+++ local.ts
@@ -1,7 +1,17 @@
 import app from './src/app';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
+
+
 
 // no need for env variable as this is only needed for local dev
 const PORT = 5000;
+
+const configPath = '/src/config/config.yml';
+const openapi = yaml.load(fs.readFileSync(__dirname + configPath, 'utf8'));
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapi));
 
 app.listen(PORT, () => {  // initialize server
   console.log(`I am up and running at http://localhost:${PORT}`);
