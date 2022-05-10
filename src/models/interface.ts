a79ebe8bc0b83743f2298f5811981e59ec790589
Fav8 <leonardo.damico7597@gmail.com>
2022-05-10 16:40:44 +0200

    feat-dataManipulation flow

--- src/models/interface.ts
+++ src/models/interface.ts
@@ -10,3 +10,9 @@ export interface IEmotion {
      Type: string,
      Confidence: number
  }
+
+export interface IFrameInfo {
+  amountOfPeople : number,
+  attentionScore: number,
+  moodScore: number
+}
