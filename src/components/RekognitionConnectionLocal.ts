98c985d77fc7d82fe3094fa7d43a7f00d9ae95a6
Fav8 <leonardo.damico7597@gmail.com>
2022-05-12 09:51:22 +0200

    fix rekonConnectionLocal

--- src/components/RekognitionConnectionLocal.ts
+++ src/components/RekognitionConnectionLocal.ts
@@ -2,6 +2,9 @@ import Configurator from './Configurator';
 import {Rekognition} from 'aws-sdk';
 import BaseRekognitionConnection from './BaseRekognitionConnection';
 import * as mockResponse1 from '../../tests/mockResponses/mockResponse1.json';
+import * as mockResponse2 from '../../tests/mockResponses/mockResponse2.json';
+import * as mockResponse3 from '../../tests/mockResponses/mockResponse3.json';
+import * as mockResponse4 from '../../tests/mockResponses/mockResponse4.json';
 import {DetectFacesResponse} from 'aws-sdk/clients/rekognition';
 
 export default class RekognitionConnectionLocal extends BaseRekognitionConnection {
@@ -13,14 +16,16 @@ export default class RekognitionConnectionLocal extends BaseRekognitionConnectio
 
   getClient (): Rekognition {
     return {
-      detectFaces: (image) => {
+      detectFaces: (params) => {
         const allMocks = {
-          mockResponse1,
+          'image.jpg': mockResponse1,
+          'image2.jpg': mockResponse2,
+          'image3.jpg': mockResponse3,
+          'image4.jpg': mockResponse4,
         };
-
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        const response = allMocks[filename].default;
+        const response = allMocks[params.Image.S3Object.Name].default;
         return response as DetectFacesResponse;
       }
     } as Rekognition;
