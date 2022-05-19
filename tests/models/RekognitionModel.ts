4433f0cb91bdbdcdb7704255669b586cd74e06cd
Tina <tina.zhou.hui@gmail.com>
2022-05-19 10:57:49 +0100

    feat: test for lower confidence faces

--- tests/models/RekognitionModel.ts
+++ tests/models/RekognitionModel.ts
@@ -10,7 +10,7 @@ describe('Rekognition Model', () => {
     const rekognitionModel = container.get('rekognitionModel') as RekognitionModel;
 
     const output4 = await rekognitionModel.detectFaces('rekogTest/1.jpg');
-    expect(output4.FaceDetails).toHaveLength(4); // this is because of mock data showing 4 faces!
+    expect(output4.FaceDetails).toHaveLength(5); // this is because of mock data showing 5 faces!
     const output3 = await rekognitionModel.detectFaces('rekogTest/2.jpg');
     expect(output3.FaceDetails).toHaveLength(3); // this is because of mock data showing 3 faces!
   });
